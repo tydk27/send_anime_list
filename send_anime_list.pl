cb8dbb157c6556bf04ee926137b00e02dfab8a82
tydk27 <tydk27@gmail.com>
2016-04-03 09:41:04 +0900

    add some function
    also bugfix

--- send_anime_list.pl
+++ send_anime_list.pl
@@ -20,35 +20,67 @@ my $xml = XML::Simple->new;
 my $response = $ua->get($api);
 
 if ($response->is_success) {
-    my $html = $response->content;
-    my $data = $xml->XMLin($html);
+    my $xml_data = $response->content;
+    my $data = $xml->XMLin($xml_data);
 
+    my $cnt = $#{$data->{ProgItems}->{ProgItem}};
     my $body = "本日放送予定のアニメ一覧\n\n";
+    my $new_flg = 0;
 
-    for (my $i = 0; $i < $#{$data->{ProgItems}->{ProgItem}} ; $i++) {
+    my @new_arr;
+
+    for (my $i = 0; $i <= $cnt; $i++) {
         my $rItme = $data->{ProgItems}->{ProgItem}->[$i];
-        my $StTime = check_date($rItme->{StTime});
-        my $EdTime = check_date($rItme->{EdTime});
-        $body .= "【$StTime～$EdTime】";
-        $body .= "$rItme->{ChName}\n";
-        $body .= "$rItme->{Title} ";
-        unless ($rItme->{Count} eq "") {
-            $body .= "第$rItme->{Count}話";
-        }
-        if ($rItme->{SubTitle} eq "") {
-            $body .= "「サブタイトル未定」";
-        } else {
-            $body .= "「$rItme->{SubTitle}」";
-        }
-        if ($rItme->{ProgComment} eq "") {
-            $body .= "\n\n";
-        } else {
-            $body .= "\n※$rItme->{ProgComment}\n\n";
+        if ($rItme->{Count} eq "1") {
+            if ($new_flg == 0) {
+                $new_flg = 1;
+                $body .= "********** 初回放送 **********\n\n";
+            }
+            $body .= make_body($rItme);
+            push(@new_arr, $i);
         }
     }
-    send_to_mail($body);
+
+    if ($new_flg == 1) {
+        $body .= "********** その他 **********\n\n";
+    }
+
+    for (my $i = 0; $i <= $cnt; $i++) {
+        next if grep {$_ eq $i} @new_arr;
+        $body .= make_body($data->{ProgItems}->{ProgItem}->[$i]);
+    }
+
+    #print $body;
+    exit 0 if send_to_mail($body) == 0;
+    exit 1;
+
 } else {
     die $response->status_line;
+}
+
+sub make_body {
+    my $rItme = shift;
+    my $body;
+
+    my $StTime = check_date($rItme->{StTime});
+    my $EdTime = check_date($rItme->{EdTime});
+    $body .= "【$StTime～$EdTime】 ";
+    $body .= "$rItme->{ChName}\n";
+    $body .= "$rItme->{Title} ";
+    unless ($rItme->{Count} eq "") {
+        $body .= "第$rItme->{Count}話";
+    }
+    if ($rItme->{SubTitle} eq "") {
+        $body .= "「サブタイトル未定」";
+    } else {
+        $body .= "「$rItme->{SubTitle}」";
+    }
+    if ($rItme->{ProgComment} eq "") {
+        $body .= "\n\n";
+    } else {
+        $body .= "\n※$rItme->{ProgComment}\n\n";
+    }
+    return $body;
 }
 
 sub send_to_mail {
@@ -84,7 +116,11 @@ sub send_to_mail {
     eval {
         sendmail($email, { 'transport' => $smtp });
     };
-    print $@;
+    if ($@) {
+        print $@;
+        return 0;
+    }
+    return 1;
 }
 
 sub check_date {
@@ -93,5 +129,3 @@ sub check_date {
     my $min = substr($time, 10, 2);
     return "$hour:$min";
 }
-
-1;
